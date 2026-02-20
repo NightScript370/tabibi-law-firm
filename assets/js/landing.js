5894c8fd0b2e77d1b4618d76de6016668a79c9fa
NightScript <18664762+NightScript370@users.noreply.github.com>
2026-02-19 20:15:44 -0500

    Tabibi Law Firm

---
+++ assets/js/landing.js
@@ -0,0 +1,61 @@
+
+var observer = new IntersectionObserver(onIntersection, {
+    root: null,   // default is the viewport
+    threshold: [0.1, 0.32, 0.52, 0.8, 1] // percentage of target's visible area. Triggers "onIntersection"
+})
+
+const navBarElem = document.getElementById('mainNav');
+const navBarCollapse = document.getElementById('navbarResponsive');
+const logo = document.getElementById('logo');
+
+// callback is called on intersection change
+function onIntersection(entries, opts){
+    if (!navBarElem) {
+        return;
+    }
+
+    entries.forEach(entry => {
+        if (entry.intersectionRatio == 1) {
+            navBarElem.classList.remove('navbar-landing-shrink')
+            navBarElem.classList.add('navbar-landing-expand')
+            logo.classList.remove('stepO')
+            return;
+        }
+
+        navBarElem.classList.add('navbar-landing-shrink')
+        navBarElem.classList.remove('navbar-landing-expand')
+        logo.classList.add('stepO')
+    })
+}
+
+// Use the observer to observe an element
+observer.observe( document.getElementById('fakeLogo') )
+
+// Collapse responsive navbar when toggler is visible
+const navbarToggler = document.body.querySelector('.navbar-toggler');
+const responsiveNavItems = [].slice.call(
+    document.querySelectorAll('#navbarResponsive .nav-link')
+);
+responsiveNavItems.map(function (responsiveNavItem) {
+    responsiveNavItem.addEventListener('click', () => {
+        if (window.getComputedStyle(navbarToggler).display !== 'none') {
+            navbarToggler.click();
+        }
+    });
+});
+
+navBarCollapse.addEventListener('show.bs.collapse', () => { logo.style.width = '30vw'; logo.style.top = '.5em'; })
+navBarCollapse.addEventListener('hide.bs.collapse', () => { ['width', 'top'].forEach(prop => logo.style.removeProperty(prop)); })
+
+if (document.readyState !== 'loading') {
+    postInit();
+} else {
+    window.addEventListener('DOMContentLoaded', event => postInit());
+}
+
+function postInit() {
+    for (const modal of document.getElementsByClassName('modal')) {
+        modal.addEventListener('hide.bs.modal', () => logo.style.removeProperty('display'));
+        modal.addEventListener('show.bs.modal', () => logo.style.display = 'none');
+    }
+}
